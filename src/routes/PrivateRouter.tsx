d5a872e7718bc08d19ad8a61b95a8a1372789b1b
mohitpuri-codes <mohitpuri.goswami@simformsolutions.com>
2025-06-05 13:11:24 +0530

    refactor: update TOKEN as a constant

--- src/routes/PrivateRouter.tsx
+++ src/routes/PrivateRouter.tsx
@@ -1,8 +1,9 @@
 import type { ReactNode } from "react";
 import { Navigate } from "react-router";
+import { TOKEN } from "../constants/globals.constants";
 
 function PrivateRouter({ children }: { children: ReactNode }) {
-  const token = localStorage.getItem("token");
+  const token = localStorage.getItem(TOKEN);
   if (token) return children;
   return <Navigate to={"/signup"} />;
 }
